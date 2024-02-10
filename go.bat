2e375b7753f449a23ae4298738a96d241f6abab3
Steven L Smith <ssmith@perdixsw.com>
2024-02-09 21:35:11 -0500

    Enhance go.bat and go.sh to bail out if they fail, instead of just barging on ahead and messing up other pythons and whatnot

--- go.bat
+++ go.bat
@@ -1,24 +1,37 @@
 @echo off
 setlocal
 
+rem Check if there are updates to this repository
 git pull
 
-rem Check if venv directory exists
+rem Check if venv directory exists; create if it doesn't...
 if not exist venv (
-    echo Creating virtual environment...
+    echo Creating Virtual Environment...
     python -m venv venv
+)
+
+rem ...but Bail if it still doesn't exist...
+if not exist venv (
+    echo FAILED to CREATE Virtual Environment!
+    exit /b 1
 )
 
 rem Activate the virtual environment
 call venv\Scripts\activate.bat
+if errorlevel 1 (
+    echo FAILED to ENTER Virtual Environment!
+    exit /b 1
+)
 
 rem Update requirements
+python -m pip install --upgrade pip
 pip install --upgrade -r requirements.txt
 
-rem Run the python script
+rem Run the Firmware Updater GUI
 python sanfw.py
 
 rem Deactivate the virtual environment
 deactivate
 
 endlocal
+exit /b 0
--- go.sh
+++ go.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
+# Check if there are updates to this repository
 git pull
 
-# Check if venv directory exists
+# Check if venv directory exists; create if it doesn't...
 if [ ! -d "venv" ]; then
-    echo "Creating virtual environment..."
+    echo Creating Virtual Environment...
     python3 -m venv venv
+fi
+
+# ...but Bail if it still doesn't exist...
+if [ ! -d "venv" ]; then
+    echo FAILED to CREATE Virtual Environment!
+    exit 1
 fi
 
 # Activate the virtual environment
 source venv/bin/activate
+if [ $? -ne 0 ]; then
+    echo FAILED to ENTER Virtual Environment!
+    exit 1
+fi
 
 # Update requirements
 pip install --upgrade -r requirements.txt
 
-# Run the python script
+# Run the Firmware Updater GUI
 python3 sanfw.py
 
 # Deactivate the virtual environment
